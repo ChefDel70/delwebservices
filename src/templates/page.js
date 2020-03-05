c990433df5f6155999cba548ac2dc322ba149340
Josh Del Sesto <Chefdel70@gmail.com>
2020-03-05 17:11:48 -0500

    Added import for Bootstrap's css library

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Layout from '../components/layout'
 import HeroImage from '../components/HeroImage'
+import { Button } from 'react-bootstrap';
+
 
 export default ({pageContext}) => (
   <Layout>
     <HeroImage />
+      <div>
+        <Button variant="primary">Click Here</Button>
+      </div>
   </Layout>
 );
